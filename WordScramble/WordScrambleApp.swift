1d4f8b951254179b6d52cb3f8cd01454c21fcdf2
Michele Volpato <mvolpato@users.noreply.github.com>
2020-11-25 21:30:05 +0100

    Day 29

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Michele Volpato on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
